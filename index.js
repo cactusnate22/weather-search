58988963c20f2a734347dcfc862b3af61f57c126
Nathan Ash <nash311@gmail.com>
2017-10-23 10:29:26 -0400

    css changes and animation attempt

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
-// want information returned in imperial units (fahrenheit)...
-// imperial api.openweathermap.org/data/2.5/find?q=London&units=imperial
 
 function getDataFromApi (citySearch, callback) {
   let query = {
@@ -34,8 +32,6 @@ function render (data) {
       </ul>
     </div>
   `;
-
-  // $('.one .js-search-results').html(cityData);
 }
 
 
@@ -43,7 +39,6 @@ function render (data) {
 function watchSubmit() {
   $('.js-search-area').submit(function (event) {
    event.preventDefault();
-   console.log('Form submitted');
 
    const $input = $('.js-city-search', event.currentTarget);
    const $results = $input.parent().parent().find('.js-search-results');
@@ -53,54 +48,8 @@ function watchSubmit() {
 
     $results.html(html);
    });
-
-   // const queryTarget = $(event.currentTarget).find('.js-city-search');
-   // const query = queryTarget.val();
-   // queryTarget.val("");
-   // getDataFromApi(query, render);
   });
 }
 
 $(watchSubmit);
 
-// // code to render second search area
-// function renderTwo (data) {
-//   let cityName = data.name ;
-//   let weatherDescription = data.weather[0].description;
-//   let currentTemp = data.main.temp;
-//   let minTemp = data.main.temp_min ;
-//   let maxTemp = data.main.temp_max;
-//   let humidity = data.main.humidity;
-
-
-//   let cityData = `
-//     <div>
-//       <ul>
-//       <li>The city you searched: ${cityName}</li>
-//       <li>The current weather is: ${weatherDescription}</li>
-//       <li>The current temperature(Fahrenheit) is: ${currentTemp}</li>
-//       <li>The low temperature(Fahrenheit) today is: ${minTemp}</li>
-//       <li>The high temperature(Fahrenheit) today is: ${maxTemp}</li>
-//       <li>Humidity is ${humidity} %.</li>
-//       </ul>
-//     </div>
-//   `;
-
-
-
-//   $('.two .js-search-results').html(cityData);
-
-// }
-
-
-// function watchSubmitTwo() {
-//   $('.two .js-search-area').submit(event=> {
-//    event.preventDefault();
-//    const queryTarget = $(event.currentTarget).find('.js-city-search');
-//    const query = queryTarget.val();
-//    queryTarget.val("");
-//    getDataFromApi(query, renderTwo);
-//   });
-// }
-
-// $(watchSubmitTwo);
